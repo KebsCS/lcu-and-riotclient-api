cd56972306a5478cef4063d104266cc37e5c0819
KebsCS <lukswi2000@gmail.com>
2023-08-28 18:27:44 +0200

    fixed php unauthorized access, thanks DarkIntaqt

--- proxy.php
+++ proxy.php
@@ -1,6 +1,22 @@
 <?php
 $url = $_GET['url'];
-$response = file_get_contents($url);
+$realurl = "";
+switch ($url) {
+    case 'lcu/data.json':
+        $realurl ='lcu/data.json';
+        break;
+    case 'lcu/data_info.json':
+        $realurl ='lcu/data_info.json';
+        break;
+    case 'riotclient/data.json':
+        $realurl ='riotclient/data.json';
+        break;
+    case 'riotclient/data_info.json':
+        $realurl ='riotclient/data_info.json';
+        break;
+    
+}
+$response = file_get_contents($realurl);
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo $response;
